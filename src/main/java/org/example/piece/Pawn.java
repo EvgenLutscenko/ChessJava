ba821b57e04258455ea50137f7a2ce004a2bb71d
Evgenii <evgenlutscenko@icloud.com>
2023-05-05 23:19:43 +0200

    Pawn also can move

--- src/main/java/org/example/piece/Pawn.java
+++ src/main/java/org/example/piece/Pawn.java
@@ -4,12 +4,30 @@ import org.example.Color;
 import org.example.Coordinates;
 import org.example.CoordinetesShift;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Pawn extends Piece{
     @Override
     public Set<CoordinetesShift> getPieceShifts() {
-        return null;
+        Set<CoordinetesShift> shifts = new HashSet<>();
+
+        if(this.color == Color.WHITE){
+            shifts.add(new CoordinetesShift(0, 1));
+
+            if (coordinates.rank == 2){
+                shifts.add(new CoordinetesShift(0, 2));
+            }
+        }else {
+            shifts.add(new CoordinetesShift(0, -1));
+
+            if (coordinates.rank == 7){
+                shifts.add(new CoordinetesShift(0, -2));
+            }
+        }
+
+
+        return shifts;
     }
 
     public Pawn(Coordinates coordinates, Color color) {
